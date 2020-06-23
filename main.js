896e90940a4fda107fe8d5e720f7cc791bacb29c
Usuario9342 <usuario934280@protonmail.com>
2020-06-23 17:31:27 -0300

    le agrego scrollreveal

---
+++ main.js
@@ -0,0 +1,11 @@
+//console.log("archivo levantado");
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    //console.log("funciona")
+    document.querySelector(".nav-menu").classList.toggle("show");
+
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cards', { delay: 100 });
+ScrollReveal().reveal('.banner', { delay: 100 });
